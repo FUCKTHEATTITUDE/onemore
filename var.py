99b07c2260bd0b27c8cb20334a553cf1740c9891
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-08-22 13:08:42 +0530

    Update var.py

--- var.py
+++ var.py
@@ -3,7 +3,7 @@ from heroku3 import from_key
 from os import getenv
 
 class Var:
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", None)
-    BOT_TOKEN = config("BOT_TOKEN", None)
-    SUDO = list(map(int, getenv("SUDO").split()))
+    API_ID = config("API_ID", default=12120671, cast=int)
+    API_HASH = config("API_HASH", "ecd153928b09383a2d0b4dde1a16c363")
+    BOT_TOKEN = config("BOT_TOKEN", "5492892673:AAHbaNMkZbfuYjRMaSc3H-ksxX4DFILgzuo")
+    SUDO = list(map(int, getenv("1930954213 1897103955 1980866589 1287077585 1990503115").split()))
