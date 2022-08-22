81943ade90906a528419ef52f6e3a1b417ce62b6
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-08-22 13:20:31 +0530

    Update var.py

--- var.py
+++ var.py
@@ -6,4 +6,4 @@ class Var:
     API_ID = config("API_ID", default=12120671, cast=int)
     API_HASH = config("API_HASH", "ecd153928b09383a2d0b4dde1a16c363")
     BOT_TOKEN = config("BOT_TOKEN", "5492892673:AAHbaNMkZbfuYjRMaSc3H-ksxX4DFILgzuo")
-    SUDO = list( map (int, getenv("SUDO", "1930954213 1897103955 1980866589 1287077585 1990503115").split())
+    SUDO = list(map (int, getenv("SUDO", "1930954213 1897103955 1980866589 1287077585 1990503115").split())
